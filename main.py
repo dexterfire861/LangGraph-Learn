64da2cdebde5186409730d64d8a47dd64fc17edc
Aryaan Verma <aryaanverma@Aryaans-MacBook-Pro.local>
2024-12-24 15:31:38 +0530

    Step 2

--- main.py
+++ main.py
@@ -25,9 +25,15 @@ anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
 
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620", api_key=anthropic_api_key)
 
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools)
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
 # The first argument is the unique node name
@@ -35,25 +41,70 @@ def chatbot(state: State):
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
 
+import json
+
+from langchain_core.messages import ToolMessage
+
+class BasicToolNode:
+    """ A node that runs the tools requested in the last AIMessage"""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict): 
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name = tool_call["name"],
+                    tool_call_id = tool_call["id"],
+                )
+            )   
+        return {"messages": outputs}
+
+tool_node = BasicToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+def route_tools(
+        state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    {"tools": "tools", END: END},
+)
+
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
 
-graph_builder.add_edge("chatbot", END)
-
 graph = graph_builder.compile()
-
-from IPython.display import Image, display
-
-try:
-    display(Image(graph.get_graph().draw_mermaid_png()))
-except Exception:
-    # This requires some extra dependencies and is optional
-    pass
 
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [("user", user_input)]}):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
-
 
 while True:
     try:
@@ -69,4 +120,3 @@ while True:
         print("User: " + user_input)
         stream_graph_updates(user_input)
         break
-
