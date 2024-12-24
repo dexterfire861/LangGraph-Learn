37abe4a4f41f9313ebfa3e629bdf82e562548cd1
Aryaan Verma <aryaanverma@Aryaans-MacBook-Pro.local>
2024-12-24 15:51:56 +0530

    part 3

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from typing import Annotated
+from typing import Annotated, TypedDict
 
 from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langchain_anthropic import ChatAnthropic
+import os
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+
+load_dotenv()
+anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
 
 
 class State(TypedDict):
@@ -15,17 +23,7 @@ class State(TypedDict):
 
 graph_builder = StateGraph(State)
 
-from langchain_anthropic import ChatAnthropic
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
-
-
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620", api_key=anthropic_api_key)
-
-from langchain_community.tools.tavily_search import TavilySearchResults
 
 tool = TavilySearchResults(max_results=2)
 tools = [tool]
@@ -35,65 +33,35 @@ llm_with_tools = llm.bind_tools(tools)
 def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+def run():
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
 
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
+            stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
+
+
 graph_builder.add_node("chatbot", chatbot)
 
-import json
-
-from langchain_core.messages import ToolMessage
-
-class BasicToolNode:
-    """ A node that runs the tools requested in the last AIMessage"""
-
-    def __init__(self, tools: list) -> None:
-        self.tools_by_name = {tool.name: tool for tool in tools}
-
-    def __call__(self, inputs: dict): 
-        if messages := inputs.get("messages", []):
-            message = messages[-1]
-        else:
-            raise ValueError("No message found in input")
-        outputs = []
-        for tool_call in message.tool_calls:
-            tool_result = self.tools_by_name[tool_call["name"]].invoke(
-                tool_call["args"]
-            )
-            outputs.append(
-                ToolMessage(
-                    content=json.dumps(tool_result),
-                    name = tool_call["name"],
-                    tool_call_id = tool_call["id"],
-                )
-            )   
-        return {"messages": outputs}
-
-tool_node = BasicToolNode(tools=[tool])
+tool_node = ToolNode(tools = [tool])
 graph_builder.add_node("tools", tool_node)
-
-def route_tools(
-        state: State,
-):
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-    return END
 
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    {"tools": "tools", END: END},
+    tools_condition,
 )
 
 graph_builder.add_edge("tools", "chatbot")
@@ -101,22 +69,33 @@ graph_builder.add_edge(START, "chatbot")
 
 graph = graph_builder.compile()
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+#Likely in production change this to SqliteSaver or PostgresSaver
+
+graph = graph_builder.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "1"}}
+
+user_input = "Hi there! My name is Will."
+
+# The config is the **second positional argument** to stream() or invoke()!
+events = graph.stream(
+    {"messages": [("user", user_input)]}, config, stream_mode="values"
+)
+for event in events:
+    event["messages"][-1].pretty_print()
+
+user_input = "Remember my name?"
+
+# The config is the **second positional argument** to stream() or invoke()!
+events = graph.stream(
+    {"messages": [("user", user_input)]}, config, stream_mode="values"
+)
+for event in events:
+    event["messages"][-1].pretty_print()
+
+
